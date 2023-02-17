3b7c42e5d3334009c129a829bc4700973031114c
Daniil Bavykin <daniil.bavykin.dev@gmail.com>
2023-02-17 15:56:03 +0200

    implement the Food Order project

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+import CartProvider from "./store/CartProvider";
+import Cart from "./components/Cart/Cart";
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+
+const App = () => {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
+  const closeHandler = () => {};
+
+  return (
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
+  );
+};
+
+export default App;
